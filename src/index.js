2e864f22be5e3db2d210fff220007d101e069827
= <=>
2023-01-28 21:59:27 +0200

    ...

--- src/index.js
+++ src/index.js
@@ -7,7 +7,54 @@ import 'antd/dist/antd.min.css';
 import AuthContextProvider from "./contexts/AuthContext";
 import OrderContextProvider from "./contexts/OrderContext";
 import RestaurantContextProvider from "./contexts/RestaurantContext";
+import {Amplify} from "aws-amplify";
+import awsConfig from './aws-exports';
 
+// const isLocalhost = Boolean(
+//     window.location.hostname === "localhost" ||
+//     // [::1] is the IPv6 localhost address.
+//     window.location.hostname === "[::1]" ||
+//     // 127.0.0.1/8 is considered localhost for IPv4.
+//     window.location.hostname.match(
+//         /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
+//     )
+// );
+//
+// // Assuming you have two redirect URIs, and the first is for localhost and second is for production
+// const [
+//     localRedirectSignIn,
+//     productionRedirectSignIn,
+// ] = awsConfig.oauth.redirectSignIn.split(",");
+//
+// const [
+//     localRedirectSignOut,
+//     productionRedirectSignOut,
+// ] = awsConfig.oauth.redirectSignOut.split(",");
+//
+// const updatedAwsConfig = {
+//     ...awsConfig,
+//     oauth: {
+//         ...awsConfig.oauth,
+//         redirectSignIn: (() => {
+//             console.log("\n\n ~~~~~~~~~~~~~~~~~~~~~ Attempting to SignIn ~~~~~~~~~~~~~~~~~~~~~ ")
+//
+//             if (isLocalhost) {
+//                 console.log("\n\n ~~~~~~~~~~~~~~~~~~~~~ localRedirectSignIn ~~~~~~~~~~~~~~~~~~~~~ :", JSON.stringify(localRedirectSignIn, null, 4))
+//                 return localRedirectSignIn
+//             } else {
+//                 console.log("\n\n ~~~~~~~~~~~~~~~~~~~~~ productionRedirectSignIn ~~~~~~~~~~~~~~~~~~~~~ :", JSON.stringify(productionRedirectSignIn, null, 4))
+//                 return productionRedirectSignIn
+//             }
+//         })(),
+//         redirectSignOut: isLocalhost ? localRedirectSignOut : productionRedirectSignOut,
+//     }
+// }
+// console.log("\n\n ~~~~~~~~~~~~~~~~~~~~~ updatedAwsConfig ~~~~~~~~~~~~~~~~~~~~~ :", JSON.stringify(updatedAwsConfig,null,4))
+//
+// Amplify.configure(updatedAwsConfig);
+
+
+Amplify.configure(awsConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
